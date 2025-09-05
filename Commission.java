62063fc113c66971e18ea90d6e9504953646b124
ToyBitZ00 <pa6308191@gmail.com>
2025-09-05 20:00:23 +0800

    Added main variables, some initialization of DecimalFormat, and getter and printing out of the methods calculation

--- Commission.java
+++ Commission.java
@@ -1,6 +1,25 @@
+import java.text.DecimalFormat;
+
 public class Commission {
 	public static void main(String[] args) {
+		double sales;
+		double commRate;
+		int cRate;
+		double result;
 
+		DecimalFormat dc = new DecimalFormat("0.00");
+		sales = 15000.0;
+		commRate = 0.07;
+		cRate = 3;
+
+		result = computeCommission(sales, commRate);
+		System.out.println("Result 1: " + dc.format(result));
+
+		result = computeCommission(sales);
+		System.out.println("Result 2: " + result);
+
+		result = computeCommission(sales, cRate);
+		System.out.println("Result 3: " + result);
 	}
 
 	public static double computeCommission(double sales, double rate) {
