5374b295d9df8696b92ae17c14ece254863bd583
ToyBitZ00 <pa6308191@gmail.com>
2025-09-05 19:57:12 +0800

    Added a new java class file, with a main method and three other no-static methods

---
+++ Commission.java
@@ -0,0 +1,17 @@
+public class Commission {
+	public static void main(String[] args) {
+
+	}
+
+	public static double computeCommission(double sales, double rate) {
+
+	}
+
+	public static double computeCommission(double sales) {
+
+	}
+
+	public static double computeCommission(double sales, int cRate) {
+
+	}
+}
